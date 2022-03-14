9b101d80ad90ea6a78e2cbb0775b907bd67a5a5e
Amit <amitkr24031995@gmail.com>
2022-03-14 16:31:54 +0530

    Adding CookiesAndDiffernceBetweenCookiesAndSession

---
+++ CookiesAndDiffernceBetweenCookiesAndSession/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using CookiesAndDiffernceBetweenCookiesAndSession.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CookiesAndDiffernceBetweenCookiesAndSession.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(User u)
+        {
+            if (ModelState.IsValid == true)
+            {
+                HttpCookie cookie = new HttpCookie("Username");
+                cookie.Value = u.Username;
+
+
+                HttpContext.Response.Cookies.Add(cookie);       // add cookie in browser
+                cookie.Expires = DateTime.Now.AddDays(2);      // persistant cookie(permanant cookie)
+                return RedirectToAction("Index", "Dashboard");
+            }
+            return View();
+        }
+    }
+}
---
+++ CookiesAndDiffernceBetweenCookiesAndSession/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CookiesAndDiffernceBetweenCookiesAndSession.MvcApplication" Language="C#" %>
